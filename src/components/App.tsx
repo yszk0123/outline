69d0946f93260a25646be78d160ae03af20e892e
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-01-14 10:04:08 +0900

    feat: display tree and edit text

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,98 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+type Version = '0.1.0';
+
+const VERSION: Version = '0.1.0';
+
+function copyToClipboard(data: Data) {
+  const formattedData = JSON.stringify(data, null, 2);
+  navigator.clipboard.writeText(formattedData);
+}
+
+type Tree = { id: string; text: string; children?: Tree[] };
+
+type Data = { version: Version; tree: Tree };
+
+function generateId(): string {
+  return `${Math.random() * 1000 * 1000 + Date.now()}`;
+}
+
+const initialData: Data = {
+  version: VERSION,
+  tree: { id: generateId(), text: '' },
+};
+
+interface TreeViewProps {
+  tree: Tree;
+  onChange: (tree: Tree, text: string) => void;
+}
+
+function TreeView({ tree, onChange }: TreeViewProps) {
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const text = event.currentTarget.value;
+      onChange(tree, text);
+    },
+    [tree, onChange],
+  );
+
+  return (
+    <ul>
+      <input type="text" value={tree.text} onChange={handleChange} />
+      {(tree.children || []).map((child, i) => {
+        return (
+          <li key={i}>
+            <TreeView tree={child} onChange={onChange} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function updateTree(tree: Tree, updateFn: (tree: Tree) => Tree): Tree {
+  return updateTreeInner(tree, tree.id, updateFn);
+}
+
+function updateTreeInner(
+  tree: Tree,
+  id: string,
+  updateFn: (tree: Tree) => Tree,
+): Tree {
+  if (tree.id === id) {
+    return updateFn(tree);
+  }
+
+  const children = tree.children
+    ? tree.children.map(child => {
+        return updateTreeInner(child, id, updateFn);
+      })
+    : undefined;
+
+  return {
+    ...tree,
+    children,
+  };
+}
 
 export function App() {
-  return <div>Hello, world</div>;
+  const [data, setData] = useState(initialData);
+  const handleCopy = useCallback(() => {
+    copyToClipboard(data);
+  }, [data]);
+  const handleChange = useCallback(
+    (tree: Tree, text: string) => {
+      const newTree = updateTree(tree, e => ({ ...e, text }));
+      const newData = { ...data, tree: newTree };
+      setData(newData);
+    },
+    [data],
+  );
+
+  return (
+    <div>
+      <TreeView tree={data.tree} onChange={handleChange} />
+      <button onClick={handleCopy}>Copy</button>
+    </div>
+  );
 }
